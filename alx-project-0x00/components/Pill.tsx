3dcf137f2c3f60a571dd6156f97b3d09f552b275
Njeri Kamau <faithkamau730@gmail.com>
2025-05-30 20:14:09 +0300

    Task 6 functional components(modifying the pill.tsx file)

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,15 +1,13 @@
+import { PillProps } from "@/interfaces";
 import React from 'react';
 
-interface PillProps {
-  title: string;
-}
 
 const Pill: React.FC<PillProps> = ({ title }) => {
   return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
+    <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+      <p className=" text-sm ">{title}</p>
     </div>
-  );
-};
+  )
+}
 
 export default Pill;
